e9983567cc24cc7fbedd7440fa02b144343882f5
vyko-24 <20223tn008[200~ajos>
2025-11-13 06:48:04 -0600

    feat: configuración backend

---
+++ server/aAPPla/src/main/java/mx/camila/aAPPla/AApPlaApplication.java
@@ -0,0 +1,13 @@
+package mx.camila.aAPPla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AApPlaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AApPlaApplication.class, args);
+	}
+
+}
